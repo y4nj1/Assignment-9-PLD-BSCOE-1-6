f1bf87256c3608f892c1ff691393528f7ae1f983
y4nj1 <giancarloestrella09@gmail.com>
2022-02-08 14:26:55 +0800

    Python file creation after installing FPDF.

---
+++ Assignment-9-PLD-BSCOE-1-6/resumepdfmaker.py
@@ -0,0 +1,7 @@
+# Assignment 9
+
+# PDF Resume Creator
+#	- Create a python program that will create your personal resume in PDF format
+#	- All personal details are stored in a JSON file
+#	- Your program should read the JSON file and write the details in the PDF
+#	- The output file should be: LASTNAME_FIRSTNAME.pdf
