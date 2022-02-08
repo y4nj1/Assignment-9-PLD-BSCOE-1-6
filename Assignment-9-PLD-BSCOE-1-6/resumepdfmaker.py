8b2356e9c387e9d586052dc86deecb1a70d080b6
y4nj1 <giancarloestrella09@gmail.com>
2022-02-08 14:32:55 +0800

    Added steps in processing the program.

--- Assignment-9-PLD-BSCOE-1-6/resumepdfmaker.py
+++ Assignment-9-PLD-BSCOE-1-6/resumepdfmaker.py
@@ -5,3 +5,10 @@
 #	- All personal details are stored in a JSON file
 #	- Your program should read the JSON file and write the details in the PDF
 #	- The output file should be: LASTNAME_FIRSTNAME.pdf
+
+# Steps:
+# 1. Install necessary extensions.
+# 2. Set-up both .json and .py files.
+# 3. Set-up the code that will convert the .json file into a .pdf file
+# 4. Format the final output.
+
