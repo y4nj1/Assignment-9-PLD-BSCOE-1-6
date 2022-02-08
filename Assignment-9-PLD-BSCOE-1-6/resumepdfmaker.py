e8b3f539cef70d38921a7f1c3b4389c6cb371986
y4nj1 <giancarloestrella09@gmail.com>
2022-02-08 17:18:11 +0800

    Shows the first layer of information in the PDF.

--- Assignment-9-PLD-BSCOE-1-6/resumepdfmaker.py
+++ Assignment-9-PLD-BSCOE-1-6/resumepdfmaker.py
@@ -16,11 +16,31 @@ from fpdf import FPDF
 import json
 from ctypes import alignment
 
+#header
+class PDF(FPDF):
+    def header(self):
+        self.image('demopic.jpg', 10, 8, 40)
+        self.set_font('helvetica', 'B', 28.5)
+        self.cell(0, 30, 'GIAN CARLO BIRON ESTRELLA', align = 'R', ln=1)
+        self.ln(3)
+
 #PDF Format
-pdf = FPDF('P', 'mm', "Letter")
+pdf = PDF('P', 'mm', "Letter")
 pdf.add_page() 
 
 #Read Information
 Resume_Info = open('sampleresume.json', 'r')
 PDFCreator = Resume_Info.read()
 Raw_Info = json.loads(PDFCreator)
+
+#Information
+for information in Raw_Info:
+    pdf.set_font('helvetica', 'BI', 18)
+    pdf.cell(0, 10, f"{information['header1']}", 'BI', ln=1)
+    pdf.set_font('helvetica', '', 12)
+    pdf.cell(0, 5, f"Name: {information['Name']}", align='L', ln=1)
+    pdf.cell(0, 5, f"Address: {information['Address']}", align='L', ln=1)
+    pdf.cell(0, 5, f"Contact No.: {information['Contact No.']}", align='L', ln=1)
+    pdf.cell(0, 5, f"Email: {information['Email']}", align='L', ln=1)
+
+pdf.output('ESTRELLA_GIAN CARLO.pdf')
